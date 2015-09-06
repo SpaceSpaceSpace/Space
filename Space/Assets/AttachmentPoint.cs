cc77a5b9c2ceafdb2db8692490b26881b0223e5b
Honeybunch <amt3824@g.rit.edu>
2015-09-06 18:00:15 -0400

    Customization system UI and logic is at MVP state. Attachments need fleshing out and the customized ship needs to be worked back into the playable state

--- Space/Assets/AttachmentPoint.cs
+++ Space/Assets/AttachmentPoint.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class AttachmentPoint : MonoBehaviour {
 
@@ -11,10 +13,19 @@ public class AttachmentPoint : MonoBehaviour {
 	Color startColor;
 	Color selectedColor = Color.blue;
 
+	Ship ship;
+
+	bool attaching;
+
 	// Use this for initialization
 	void Start () {
 		mat = GetComponent<Renderer>().material;
 		startColor = mat.color;
+
+		//Ship is attached to the parent game object
+		ship = transform.parent.gameObject.GetComponent<Ship>();
+
+		attaching = false;
 	}
 	
 	// Update is called once per frame
@@ -24,9 +35,42 @@ public class AttachmentPoint : MonoBehaviour {
 
 	void OnMouseOver(){
         if(WeaponToggles.AnyTogglesOn())
+		{
 		    mat.color = selectedColor;
+			Vector2 pointPos = transform.position;
+
+			if(Input.GetMouseButtonDown(0) && !attaching)
+			{
+				Toggle selectedToggle = WeaponToggles.ActiveToggles().FirstOrDefault();
+				AttachmentToggle attachmentToggle = selectedToggle.GetComponent<AttachmentToggle>();
+
+				GameObject attachment = attachmentToggle.Attachment;
+
+				//If the ship has an attachment here we should remove it
+
+				if(ship.Attachments.ContainsKey(pointPos))
+				{
+					GameObject currentAttachment = ship.Attachments[pointPos];
+					Destroy(currentAttachment);
+				}
+
+				//Now add the selected attachment to the ships' attached Weapons dictionary and instantiate it
+				GameObject attachmentClone = GameObject.Instantiate(attachment);
+				attachmentClone.transform.position = pointPos;
+				attachmentClone.transform.SetParent(ship.transform);
+				ship.Attachments[pointPos] = attachmentClone;
+
+				//Don't let this happen again until the mouse is lifted
+				attaching = true;
+			}
+			else if(Input.GetMouseButtonUp(0))
+			{
+				attaching = false;
+			}
+		}
 	}
 	void OnMouseExit(){
 		mat.color = startColor;
+		attaching = false;
 	}
 }
--- Space/Assets/Ship.cs
+++ Space/Assets/Ship.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 public class Ship : MonoBehaviour {
 
 	public List<Vector2> AttachmentPoints;
-	public Dictionary<Vector2, Weapon> AttachedWeapons;
+	public Dictionary<Vector2, GameObject> Attachments;
 
 	// Use this for initialization
 	void Start () {
-	
+		AttachmentPoints = new List<Vector2>();
+		Attachments = new Dictionary<Vector2, GameObject>();
 	}
 	
 	// Update is called once per frame
