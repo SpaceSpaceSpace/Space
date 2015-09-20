284e19192f252fea7e74cdbf989bc2f24c90ee3c
Jarrett <cjw6117@rit.edu>
2015-09-20 16:59:05 -0400

    Updated satellite class and the camera in the scene is now orthographic

--- Space/Assets/Scripts/Environment/Satellite.cs
+++ Space/Assets/Scripts/Environment/Satellite.cs
@@ -7,11 +7,11 @@ public class Satellite : MonoBehaviour {
 	public float mass;
 	public float radius;
 	public float splitForce;
-	public float GRAV_CONSTANT  = 6.673f * (1/Mathf.Pow(10,11)); 
-	public float ORBITAL_CENTER_MASS = 100.0f;
+	private float rotationFactor;
 	private Vector2 velocity;
 	private Vector2 acceleration;
 	private Transform centerOfOrbit;
+
 
 	// Use this for initialization
 	void Start () {
@@ -28,21 +28,24 @@ public class Satellite : MonoBehaviour {
 			Sprite image = Resources.Load<Sprite> (path);
 			GetComponent<SpriteRenderer> ().sprite = image;
 		}
+		mass = Random.Range (1, 9);
+		velocity = new Vector2(0, Random.Range(1, 2));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		velocity += acceleration;
+		//velocity += acceleration;
+		//transform.Rotate (0.0f, 0.0f, rotationFactor);
+		transform.Rotate (0.0f,rotationFactor, 0.0f);
+		//transform.Rotate (rotationFactor, 0.0f, 0.0f);
 		transform.Translate(velocity);
 	}
 
 	void CalculateGravitationalForce(){
 		float gravForce;
 		Vector3 distanceBetween = centerOfOrbit.position - transform.position;
-		float distance = Vector3.Distance(centerOfOrbit.position, transform.position);
-		Vector3 unitVec = distanceBetween.Normalize;
-		gravForce = -GRAV_CONSTANT * (ORBITAL_CENTER_MASS * mass)/distance * unitVec;
-		acceleration = unitVec * gravForce;
+		rotationFactor = Mathf.Sin (Time.deltaTime * 2 * Mathf.PI);
+		acceleration = distanceBetween * velocity.magnitude;
 	}
 
 	public void SetCenterOfOrbit(Transform cO){
