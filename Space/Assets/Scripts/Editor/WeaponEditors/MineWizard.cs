8e33a4216bd9e416d5af6c7566a80ff88d7e962e
Honeybunch <amt3824@g.rit.edu>
2015-09-25 18:21:29 -0400

    Fixed naming issues in Laser Wizard and flesh out Mine Wizard

--- Space/Assets/Scripts/Editor/WeaponEditors/MineWizard.cs
+++ Space/Assets/Scripts/Editor/WeaponEditors/MineWizard.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
-public class MineWizard : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class MineWizard : WizardMaster
+{
+	[Range (0,5)]
+	public float RateOfFire = 1.0f;
 	
+	public float AttackPower = 10;
+	public float ShieldPiercing = 2;
+	public float DamageRadius = 2.0f; // in meters
+	
+	[MenuItem("Space/New/Weapon/Mine")]
+	static void CreateWizard()
+	{
+		ScriptableWizard.DisplayWizard<MineWizard>("New Mine", "Create");
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void OnWizardCreate()
+	{ 
+		if(!ValidateInput())
+			return;
+		
+		//Create game object with weapon info
+		GameObject mineObject = new GameObject(Name);
+		
+		SpriteRenderer renderer = mineObject.AddComponent<SpriteRenderer>();
+		renderer.sprite = WeaponImage;
+		renderer.material = AssetDatabase.LoadAssetAtPath<Material>("Assets/Materials/WepMat.mat");
+		
+		WeaponScript mineScript = mineObject.AddComponent<WeaponScript>();
+		mineScript.projectilePrefab = ProjectilePrefab;
+		
+		mineScript.projectileSpeed = 0;
+		
+		mineScript.attackPower = AttackPower;
+		mineScript.fireTime = RateOfFire;
+		mineScript.shieldPiercing = ShieldPiercing;
+		
+		//Save game object to prefab
+		PrefabUtility.CreatePrefab(WeaponPath + Name + ".prefab", mineObject);
+		
+		//Delete game object from scene
+		DestroyImmediate(mineObject);
+	}
 	
+	protected override void ReopenWindow()
+	{
+		MineWizard newWindow = ScriptableWizard.DisplayWizard<MineWizard>("New Mine", "Create");
+		
+		newWindow.Name = Name;
+		
+		newWindow.WeaponImage = WeaponImage;
+		newWindow.ProjectilePrefab = ProjectilePrefab;
+		
+		newWindow.RateOfFire = RateOfFire;
+		
+		newWindow.AttackPower = ShieldPiercing;
+		newWindow.ShieldPiercing = ShieldPiercing;
+		newWindow.DamageRadius = DamageRadius;
 	}
 }
