c558cb2567924a01908fbd3993d2529c8b7d8483
Jarrett <cjw6117@rit.edu>
2015-09-27 17:36:39 -0400

    Starting a contracts branch

---
+++ Space/Assets/Scripts/Gameplay/Contracts/Contract.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Contract : MonoBehaviour {
+
+	public bool completed;
+	public GameObject[] objectives;
+	public GameObject[] rewards;
+	private string description;
+	private string targetImage;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public string GetDescription()
+	{
+		return description;
+	}
+	public void SetDescription(string d)
+	{
+		description = d;
+	}
+}
