2522b57e3f111f26bece0b890a98ca13f8b8a8c4
dwl5986 <dwl5986@rit.edu>
2015-10-19 01:32:04 -0400

    when leaving the zone, you are now teleported to the center of the planet. its not the space station due to the station not truly orbiting and just rotating around the center, so i have to fix that. multiple sectors can be implemented once someone schools me real quick on UI text in Unity and the interact system we currently have.

---
+++ Space/Assets/Scripts/Environment/WarpScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// This will eventually open the warp UI, for now just warps to space station
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log ("Out of Bounds!");
+		WarpToStation ();
+	}
+
+	void WarpToStation()
+	{
+		GameObject playerShip = (GameObject)GameObject.Find ("Player Ship");
+		GameObject spaceStation = (GameObject)GameObject.Find ("SpaceStoreOrbit");
+
+		StartCoroutine ("WarpWaitTime");
+
+		playerShip.transform.position = spaceStation.transform.position;
+	}
+
+	IEnumerator WarpWaitTime()
+	{
+		GUI.Label (new Rect (0, 1, 1, 1), "Warping");
+		yield return new WaitForSeconds (5.0f);
+		
+	}
+}
