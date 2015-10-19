765b9a4dd98652db09edb0fb9172cba1c1cbd8cc
JosephCoppola <jjc9718@rit.edu>
2015-10-18 22:04:41 -0400

    Removes and sets null when no contracts remain

--- Space/Assets/Scripts/UI/BountyBoard.cs
+++ Space/Assets/Scripts/UI/BountyBoard.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 
 public class BountyBoard : MonoBehaviour {
 
@@ -20,7 +21,26 @@ public class BountyBoard : MonoBehaviour {
 	{
 		currentContracts = new List<Contract> ();
 
+		/*
 		for(int i = 0; i < 10; i++)
+		{
+			Contract contract = new Contract();
+			currentContracts.Add(contract);
+		}
+		*/
+
+		currentContracts.Add (new Contract ("Joeba da Butt", "A warrant has been put out for the destruction of $name, The $title. This criminal has been found guilty of crimes against space. Their destruction of dozens of space mines have killed thousands. Kill this target and return to $thissector for reward.",
+		                                  "Space Slug Gangsta", "190,000 Grappels"));
+		currentContracts.Add (new Contract ("Fart Face McGee", "He terrorized a sub-intelligent species, teach him a lesson",
+		                                    "Galatic Gangster", "80,000 Space Dollars"));
+		currentContracts.Add (new Contract ("Hokun the Broad", "You must hunt down this monster gangster and his crew",
+		                                    "Crimepunk", "19,000 Space Dollars"));
+		currentContracts.Add (new Contract ("Gadarble", "A bounty has been put on the head of The $title $name. This criminal has been found guilty of antennae mutilation across the galaxy. Target is dangerous: do not take alive.",
+		                                    "Space Slug Gangsta", "15,000 Space Dollars"));
+		currentContracts.Add (new Contract ("Skinface", "New, evolving life has been found in $randomsector. This new species must be protected under the Galactic Code. However, The $title $name has been caught rampantly destroying and torturing this new race. Target must be liquidated.",
+		                                    "Eyebrow Waggler", "12,500 Space Dollars"));
+
+		for(int i = 0; i < 5; i++)
 		{
 			Contract contract = new Contract();
 			currentContracts.Add(contract);
@@ -40,6 +60,8 @@ public class BountyBoard : MonoBehaviour {
 			button.gameObject.GetComponent<Button>().onClick.AddListener(()=>SetBountyValues(_i));
 			button.GetComponentInChildren<Text>().text = currentContracts[i].Name;
 		}
+
+		SetBountyValues (0);
 	}
 
 	public void DestroyButtons()
@@ -58,6 +80,18 @@ public class BountyBoard : MonoBehaviour {
 		GameObject button = scrollView.transform.FindChild ("contract" + currentSelectedContract).gameObject;
 
 		Destroy (scrollView.transform.FindChild("contract" + currentSelectedContract).gameObject);
+
+		try
+		{
+			string indexString = scrollView.transform.GetChild(1).name;
+			int index = int.Parse(indexString[indexString.Length-1].ToString());
+
+			SetBountyValues (index);
+		}
+		catch(Exception e)
+		{
+			SetBlankValues();
+		}
 	}
 
 	public void SetBountyValues(int index)
@@ -69,6 +103,14 @@ public class BountyBoard : MonoBehaviour {
 		SetTitle (values ["Title"]);
 		SetDescription (values ["Description"]);
 		SetReward (values ["Reward"]);
+	}
+
+	private void SetBlankValues()
+	{
+		SetName ("-----");
+		SetTitle ("-----");
+		SetDescription ("-----");
+		SetReward("-----");
 	}
 
 	public void SetName(string p_Name)
