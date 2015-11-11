855e69805c8e0ebcd777d555f6c096aacc2e3ad1
JosephCoppola <jjc9718@rit.edu>
2015-11-11 18:50:01 -0500

    Inventory start

---
+++ Space/Assets/Scripts/Gameplay/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory
+{
+	private List<WeaponInfo> weapons;
+
+	public List<WeaponInfo> Weapons
+	{
+		get { return weapons; }
+	}
+
+	public void AddWeapon( WeaponInfo weaponToAdd )
+	{
+		weapons.Add( weaponToAdd );
+	}
+
+	public void RemoveWeapon( WeaponInfo weaponToRemove )
+	{
+		weapons.Remove( weaponToRemove );
+	}
+}
