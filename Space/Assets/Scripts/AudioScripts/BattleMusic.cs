b2f657222bc0dd41ad226e3217e03054a0bad40d
Jarrett Way <cjw6117@rit.edu>
2015-11-17 17:45:40 -0500

    Working on transitioning to battles

---
+++ Space/Assets/Scripts/AudioScripts/BattleMusic.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class BattleMusic : MonoBehaviour {
+/* 
+ *  Based on CombatMusicControl found in the Unity Tutorial: 
+ *  Audio/Adding Music To Your Game 
+ * 
+ * 
+ */
+
+
+    public AudioMixerSnapshot outOfCombat;
+    public AudioMixerSnapshot inCombat;
+    public AudioClip[] transitionInstruments;
+    public AudioSource transitionSource;
+    public float bpm = 128;
+
+
+    private float m_TransitionIn;
+    private float m_TransitionOut;
+    private float m_QuarterNote;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+        m_TransitionOut = m_QuarterNote * 32;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ship"))
+        {
+            inCombat.TransitionTo(m_TransitionIn);
+            //PlayTransitionClip();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ship"))
+        {
+            outOfCombat.TransitionTo(m_TransitionOut);
+        }
+    }
+
+    void PlayTransitionClip()
+    {
+        int randClip = Random.Range(0, transitionInstruments.Length);
+        transitionSource.clip = transitionInstruments[randClip];
+        transitionSource.Play();
+    }
+
+}
