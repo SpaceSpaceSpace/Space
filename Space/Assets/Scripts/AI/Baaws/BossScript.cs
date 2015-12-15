3272bc1bde9cc117fb80fbbe6a9deaba99669c3e
Nick Conlon <ncon67@gmail.com>
2015-12-14 22:38:03 -0500

    Boss fight stuffs

---
+++ Space/Assets/Scripts/AI/Baaws/BossScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossScript : MonoBehaviour
+{
+	public GameObject turretContainer;
+	public GameObject explosion;
+
+	private int m_generatorCount;
+
+	void Start ()
+	{
+		m_generatorCount = turretContainer.transform.childCount; // 1 generator per turret container
+		EventManager.AddEventListener( "GeneratorDestroyed", OnGeneratorDestroyed );
+	
+	}
+	
+	private void OnGeneratorDestroyed()
+	{
+		m_generatorCount--;
+		if( m_generatorCount <= 0 )
+		{
+			BoomBoom();
+		}
+	}
+
+	private void BoomBoom()
+	{
+		for( int i = 0; i < 20; i++ )
+		{
+			StartCoroutine( SpawnExplosion() );
+		}
+	}
+
+	private IEnumerator SpawnExplosion()
+	{
+		yield return new WaitForSeconds( Random.Range( 0.0f, 5.0f ) );
+		Instantiate( explosion, Random.insideUnitCircle * 20, Quaternion.identity );
+	}
+}
---
+++ Space/Assets/Scripts/AI/Baaws/ShieldGeneratorScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldGeneratorScript : ShipScript
+{
+	public Sprite destroyedSprite;
+
+	protected override void Die()
+	{
+		EventManager.TriggerEvent( "GeneratorDestroyed" );
+
+		GetComponent<SpriteRenderer>().sprite = destroyedSprite;
+		GetComponent<CircleCollider2D>().enabled = false;
+		Instantiate(m_exploder, transform.position, Quaternion.identity);
+	}
+}
