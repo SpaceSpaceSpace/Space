2d1bfaa3a09b8f155048d400121ed6803298ce4e
Jarrett Way <cjw6117@rit.edu>
2015-12-16 11:39:27 -0500

    Added another exploration and battle audio snapshot so there are two background music tracks for each state

--- Space/Assets/Scripts/AudioScripts/MusicManager.cs
+++ Space/Assets/Scripts/AudioScripts/MusicManager.cs
@@ -15,6 +15,8 @@ public class MusicManager : MonoBehaviour {
     public AudioMixerSnapshot inCombat;
     public AudioMixerSnapshot transitionToBattle;
     public AudioMixerSnapshot transitionFromBattle;
+    public AudioMixerSnapshot outOfCombat2;
+    public AudioMixerSnapshot inCombat2;
     public float bpm = 128;
 
 
@@ -35,21 +37,27 @@ public class MusicManager : MonoBehaviour {
 	}
     void PlayTransitionIntoBattle()
     {
-        transitionToBattle.TransitionTo(m_TransitionIn);
+        transitionToBattle.TransitionTo(0f);
     }
 	void PlayTransitionOutOfBattle()
 	{
-        transitionFromBattle.TransitionTo(m_TransitionIn);
+        transitionFromBattle.TransitionTo(0f);
 	}
     public void TransitionToCombat()
     {
         PlayTransitionIntoBattle();
-        inCombat.TransitionTo(m_TransitionIn * 2.0f);
+        if (Random.Range(0, 1) <= 0.5)
+            inCombat.TransitionTo(m_TransitionIn * 2.0f);
+        else
+            inCombat2.TransitionTo(m_TransitionIn * 2.0f);
         //PlayTransitionClip();
     }
     public void TransitionToExploration()
     {
         PlayTransitionOutOfBattle();
-        outOfCombat.TransitionTo(m_TransitionOut);
+        if (Random.Range(0, 2) <= 0.5)
+            outOfCombat.TransitionTo(m_TransitionOut);
+        else
+            outOfCombat2.TransitionTo(m_TransitionOut);
     }
 }
